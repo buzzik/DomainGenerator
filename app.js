a3e72e3293462efad0b222621e6b345106046b31
mr4k <buzzik13@gmail.com>
2021-02-18 23:03:14 +0200

    Refactoring done

--- app.js
+++ app.js
@@ -1,84 +1,30 @@
 const credentials = require('./credentials.js');
-const fs = require("fs");
-const util = require("util");
+
+
 const path = require("path");
 const prompt = require('prompt-sync')();
 const got = require('got');
-const process = require('process');
-class DomainGenerator {
-    constructor() {
-        this.inputFile1 = "input_1.txt";
-        this.inputFile2 = "input_2.txt";
-        this.optMaxLength = 8;
-        this.optFirstPartLength = undefined;
-        // this.secondPartLength = undefined; //not yet implemented
-        this.optDomainZone = '.com';
-        this.optTwoways = 'n';
-        this.resArr = [];
-        this.firstArr = [];
-        this.secondArr = [];
-        this.resFileName = "";
-        this._counter = 0;
-    }
-    init() {
-        let now = new Date();
-        this.resFileName = `./result/result_${now.getTime()}.txt`;
-        fs.writeFile(this.resFileName, '', (err) => {
-            if (err) throw err;
-            console.log('\nThe file has been saved!');
-        });
-        this.firstArr = this.readFiles(this.inputFile1);
-        this.secondArr = this.readFiles(this.inputFile2);
-        this.mergeParts();
-        // let self = this;
-        // this.resArr.forEach(function(domain, i) {
-        //     self.getDomainInfo(domain);
+const DomainChecker = require('./src/DomainChecker.js');
+const DomainGenerator = require('./src/DomainGenerator.js');
+const FileWriter = require('./src/FileWriter.js');
 
+const generator = new DomainGenerator();
+const checker = new DomainChecker(credentials.secret, credentials.key);
+const exporter = new FileWriter();
+generator.optMaxLength = prompt(`Enter Max Length limit (8) :`, 8);
+generator.optFirstPartLength = prompt(`Enter first part Length  (flexible) :`);
+generator.optDomainZone = prompt(`Enter Domain Zone (.com) :`, '.com');
+generator.optTwoways = prompt(`Try both directions?(first+second and second+first) (y/n default n) :`, 'n');
+
+try {
+    let rawArr = generator.init();
+    let checkedArr = checker.groupCheck(rawArr);
+    checkedArr.then((result) => {
+        exporter.writeArray(result);
+        // result.forEach(element => {
+        //     console.log(`${element.domain} - ${element.available}`)
         // });
-    }
-    readFiles(filepath, progress) {
-        var data = fs.readFileSync(filepath, 'utf8');
-        var content = util.format(data);
-        let array = content.split(/\r?\n|\r/g);
-        return array;
-    }
-    mergeParts() {
-        this.secondArr.forEach(secondPart => {
-            if (secondPart.length < this.optMaxLength) {
-                let allowedLength = this.optMaxLength - secondPart.length;
-                for (const firstPart of this.firstArr) {
-                    if (this.optFirstPartLength && this.optFirstPartLength == firstPart.length || !this.optFirstPartLength && firstPart.length <= allowedLength) {
-                        this.resArr.push(capitalizeFirstLetter(firstPart) + capitalizeFirstLetter(secondPart) + this.optDomainZone);
-                        if (this.optTwoways == "y") {
-                            this.resArr.push(capitalizeFirstLetter(secondPart) + capitalizeFirstLetter(firstPart) + this.optDomainZone);
-                        }
-                    }
-                }
-            }
-        });
-    }
-
+    });
+} catch (e) {
+    console.log(e);
 }
-
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-
-function drawProgress(i, size) {
-    process.stdout.clearLine();
-    process.stdout.cursorTo(0);
-    process.stdout.write(`Processing : ${size} / ${i}`);
-}
-
-function processArray(array, iterator) {
-
-}
-let Generator = new DomainGenerator();
-
-Generator.optMaxLength = prompt(`Enter Max Length limit (8) :`, 8);
-Generator.optFirstPartLength = prompt(`Enter first part Length  (flexible) :`);
-Generator.optDomainZone = prompt(`Enter Domain Zone (.com) :`, '.com');
-Generator.optTwoways = prompt(`Try both directions?(first+second and second+first) (y/n default n) :`, 'n');
-
-Generator.init();
--- src/DomainChecker.js
+++ src/DomainChecker.js
@@ -1,4 +1,5 @@
 const got = require('got');
+const process = require('process');
 module.exports = class DomainChecker {
     constructor(secret, key) { //You need to provide GoDaddy api Key and Secret
         this.options = {
@@ -16,17 +17,28 @@ module.exports = class DomainChecker {
     async checkDomain(domain) {
         this.options.searchParams.domain = domain;
         // const body = await got('https://api.godaddy.com/v1/domains/available', this.options).json();
-        return got('https://api.godaddy.com/v1/domains/available', this.options).json();
-
+        return got('https://api.ote-godaddy.com/v1/domains/available', this.options).json();
     }
-    async groupCheck(array, this.writeResult) {
-        let results;
-        for (const url of urls) {
-            results = await checkDomain(url, results);
+    async groupCheck(array, callback) {
+        let results = [];
+        let res;
+        let iterator = 0;
+        for (const url of array) {
+
+            let res = await this.checkDomain(url);
+            results.push(res);
+            iterator++;
+            drawProgress(iterator, array.length);
+
         }
-        callback(result);
-    }
-    writeResult() {
-
+        console.log(`\nDone.`);
+        return results;
+        // callback(result);
     }
 };
+
+function drawProgress(i, size) {
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write(`Processing : ${size} / ${i}`);
+}
