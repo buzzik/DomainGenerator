fc30d8fdb98aaf1a7793d4a687184e5380845851
mr4k <buzzik13@gmail.com>
2021-02-18 13:05:32 +0200

    refactoring

--- src/Generator.js
+++ src/Generator.js
@@ -1,4 +1,3 @@
-const credentials = require('./credentials.js');
 const fs = require("fs");
 const util = require("util");
 const path = require("path");
@@ -7,8 +6,8 @@ const https = require('https');
 
 class DomainGenerator {
     constructor() {
-        this.inputFile1 = "input_1.txt";
-        this.inputFile2 = "input_2.txt";
+        const firstPart = "input_1.txt";
+        const secondPart = "input_2.txt";
         this.optMaxLength = 8;
         this.optFirstPartLength = undefined;
         // this.secondPartLength = prompt(`Enter second part Length  (flexible) :`); /not yet implemented
@@ -18,7 +17,6 @@ class DomainGenerator {
         this.firstArr = [];
         this.secondArr = [];
         this.resFileName = "";
-        this.init();
     }
     init() {
         this.optMaxLength = prompt(`Enter Max Length limit (8) :`, 8);
@@ -28,15 +26,13 @@ class DomainGenerator {
         this.optTwoways = prompt(`Try both directions?(first+second and second+first) (y/n default n) :`, 'n');
         let now = new Date();
         this.resFileName = `./result/result_${now.getTime()}.txt`;
-        fs.writeFile(this.resFileName, '', (err) => {
+        fs.writeFile(resFileName, '', (err) => {
             if (err) throw err;
             console.log('The file has been saved!');
         });
-        this.firstArr = this.readFiles(this.inputFile1);
-        this.secondArr = this.readFiles(this.inputFile2);
-        this.mergeParts();
-        let self = this;
-        this.resArr.forEach(function(domain) { self.getDomainInfo(domain); });
+        this.firstArr = this.readFiles(firstPart);
+        this.secondArr = this.readFiles(secondPart);
+        this.resArr.forEach(function(domain) { getDomainInfo(domain); });
     }
 
     readFiles(filepath, progress) {
@@ -47,13 +43,13 @@ class DomainGenerator {
     }
     mergeParts() {
         this.secondArr.forEach(secondPart => {
-            if (secondPart.length < this.optMaxLength) {
-                let allowedLength = this.optMaxLength - secondPart.length;
+            if (secondPart.length < optMaxLength) {
+                let allowedLength = optMaxLength - secondPart.length;
                 for (const firstPart of this.firstArr) {
                     if (this.optFirstPartLength && this.optFirstPartLength == firstPart.length || !this.optFirstPartLength && firstPart.length <= allowedLength) {
-                        this.resArr.push(capitalizeFirstLetter(firstPart) + capitalizeFirstLetter(secondPart) + this.optDomainZone);
-                        if (this.optTwoways == "y") {
-                            this.resArr.push(capitalizeFirstLetter(secondPart) + capitalizeFirstLetter(firstPart) + this.optDomainZone);
+                        this.resArr.push(capitalizeFirstLetter(firstPart) + capitalizeFirstLetter(secondPart) + optDomainZone);
+                        if (optTwoways == "y") {
+                            this.resArr.push(capitalizeFirstLetter(secondPart) + capitalizeFirstLetter(firstPart) + optDomainZone);
                         }
                     }
                 }
@@ -78,7 +74,7 @@ class DomainGenerator {
             res.on('data', (body) => {
                 let fbResponse = JSON.parse(body);
                 if (fbResponse.available) {
-                    fs.appendFile(this.resFileName, `${domain}\t ${fbResponse.price/1000000}\n`, (err) => {
+                    fs.appendFile(resFileName, `${domain}\t ${fbResponse.price/1000000}\n`, (err) => {
                         if (err) throw err;
                         // console.log('The "data to append" was appended to file!');
                     });
@@ -96,4 +92,7 @@ class DomainGenerator {
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
-new DomainGenerator();
+module.exports = {
+    DomainGenerator,
+    init
+}
